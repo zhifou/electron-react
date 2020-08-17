b10c7dfb356d9753dce1fd4b1b7cba4d610b475c
zhaoyadong <zhaoyadong@baidu.com>
2020-08-17 20:39:39 +0800

    修改了配置

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow, ipcMain, Notification } = require('electron')
+const path = require('path');
+
+const isDev = process.env.ENV === 'development';
+
+function createWindow() {
+  // 创建浏览器窗口
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  if (isDev) {
+    win.loadURL(`http://localhost:3000`);
+  } else {
+    win.loadFile(path.resolve(__dirname, './dist/index.html'));
+  }
+
+    // 打开开发者工具，默认不打开
+    win.webContents.openDevTools();
+}
+
+// 监听渲染程序发来的事件
+ipcMain.on('something', (event, data) => {
+  console.log('接收到render进程发送的消息', data);
+  let myNotification = new Notification({ title: '主进程通知', body: '接收到render进程发送的消息' });
+  myNotification.show();
+  event.sender.send('something1', '我是主进程返回的值')
+})
+
+app.whenReady().then(createWindow)
