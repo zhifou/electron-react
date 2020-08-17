d8ce9d38710ad9c76a0f8c6061a24e75aad2a88d
zhaoyadong <zhaoyadong@baidu.com>
2020-08-17 11:12:58 +0800

    首次签入electron工程

---
+++ src/container/App/index.tsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { remote, ipcRenderer } from 'electron';
+import './index.scss';
+import logo from '../../logo.svg';
+import download from '../../download.png';
+
+const { Notification } = remote;
+
+class App extends Component {
+  componentDidMount() {
+    // 监听主进程发来的事件
+    ipcRenderer.on('something1', (event: any, data: any) => {
+      console.log('接收到main进程发送的消息', data); // 我是主进程返回的值
+    })
+  }
+
+  onShowNotification() {
+    let myNotification = new Notification({ title: '渲染进程通知', body: '在渲染进程中直接使用主进程的模块' });
+    myNotification.show();
+  }
+
+  onSendMessageToMain() {
+    // 发送事件给主进程
+    ipcRenderer.send('something', '传输给主进程的值')
+  }
+
+  componentWillUnmount() {
+    ipcRenderer.removeAllListeners('something1');
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={download} className="App-download" alt="download" />
+        <button onClick={this.onSendMessageToMain}>与主进程通信</button>
+        <br /> <br />
+        <button onClick={this.onShowNotification}>使用 remote 直接调用主进程模块</button>
+      </React.Fragment>
+    )
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const spawn = require('child_process').spawn;
+
+module.exports = {
+  target: 'electron-renderer',
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.(eot|ttf)$/,
+        use: {
+          loader: 'file-loader',
+          options: {  // 配置
+            name: '[name].[ext]', // 以源文件名字及格式输出
+            outputPath: 'images/' // 输出到images文件夹下
+          }
+        }
+      },
+      {
+        test: /\.(svg|jpg|png|gif)$/,
+        use: {
+          loader: 'url-loader',
+          options: { // 配置
+            name: '[name].[ext]', // 以源文件名字及格式输出
+            outputPath: 'images/', // 输出到images文件夹下
+            limit: 10240 // 超过10kb打包为图片
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    port: 3000,
+    after() {
+      spawn('npm', ['run', 'start-electron'], {
+        shell: true,
+        env: process.env,
+        stdio: 'inherit'
+      })
+        .on('close', code => process.exit(code))
+        .on('error', spawnError => console.error(spawnError));
+    }
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      "@container": path.resolve(__dirname, 'src/container/')
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ]
+}
