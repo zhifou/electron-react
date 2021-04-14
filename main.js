ef45a92120d00c133bd1a5e0929bdd2b1fb4ad3b
zhaoyadong <zhaoyadong@baidu.com>
2021-04-14 10:14:52 +0800

    增加一些功能

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-const { app, BrowserWindow, ipcMain, Notification } = require('electron')
+const { app, BrowserWindow, ipcMain, Notification, Menu } = require('electron')
 const electron = require('electron');
 const path = require('path');
 //用一个 Tray 来表示一个图标,这个图标处于正在运行的系统的通知区 ，通常被添加到一个 context menu 上.
-const Menu = electron.Menu;
+// const Menu = electron.Menu;
 const Tray = electron.Tray;
+const globalShortcut = electron.globalShortcut;
+
 //托盘对象
 let appTray = null;
 
@@ -15,23 +17,30 @@ function createWindow() {
     win = new BrowserWindow({
         show: true,
         width: 890,
-        height: 556,
+        height: 600,
         webgl: true,
         frame: true,
         transparent: false,
+        // titleBarStyle: 'hiddenInset',
         webPreferences: {
             webSecurity: true,
             allowRunningInsecureContent: false,
             nativeWindowOpen: false,
-            nodeIntegration: true
+            nodeIntegration: true,
+
+            contextIsolation: false,
+            worldSafeExecuteJavaScript: false,
+            nodeIntegrationInWorker: true,
+            webviewTag: true, // 解决webview无法显示问题
+            enableRemoteModule: true
         },
         backgroundColor: '#efefef',
-        // icon: path.join(__dirname, 'assets/images/app.png')
+        icon: path.join(__dirname, 'assets/images/app.png')
     });
 
-    // if (process.platform === 'darwin') {
-    //    app.dock.setIcon(path.join(__dirname, 'assets/images/app.png'));
-    // }
+    if (process.platform === 'darwin') {
+        app.dock.setIcon(path.join(__dirname, 'assets/images/app.png'));
+    }
 
     if (isDev) {
         win.loadURL(`http://localhost:3000`);
@@ -66,27 +75,70 @@ function createWindow() {
         }
     ];
 
-    // //系统托盘图标目录
-    // let trayIcon = path.join(__dirname, 'assets/images');//app是选取的目录
+    // 系统托盘图标目录
+    let trayIcon = path.join(__dirname, 'assets/images');//app是选取的目录
 
-    // appTray = new Tray(path.join(trayIcon, 'app.ico'));//app.ico是app目录下的ico文件
+    appTray = new Tray(path.join(trayIcon, 'app.ico'));//app.ico是app目录下的ico文件
 
-    // //图标的上下文菜单
-    // const contextMenu = Menu.buildFromTemplate(trayMenuTemplate);
+    // 图标的上下文菜单
+    const contextMenu = Menu.buildFromTemplate(trayMenuTemplate);
 
-    // //设置此托盘图标的悬停提示内容
-    // appTray.setToolTip('我的托盘图标');
+    //设置此托盘图标的悬停提示内容
+    appTray.setToolTip('我的托盘图标');
 
-    // //设置此图标的上下文菜单
-    // appTray.setContextMenu(contextMenu);
+    // 设置此图标的上下文菜单
+    appTray.setContextMenu(contextMenu);
 
-    // //单击右下角小图标显示应用
-    // appTray.on('click', function () {
-    //     win.show();
-    // })
+    // 单击右下角小图标显示应用
+    appTray.on('click', function () {
+        win.show();
+    });
+
+    // 设置应用程序主菜单
+    let template = [
+        {
+            label: '乐库',
+            submenu: [
+                {
+                    label:'儿童大全',
+                    click:() => {
+                        let win = new BrowserWindow({
+                            width:500,
+                            height:500,
+                            webPreferences: {
+                                nodeIntegration: true
+                            }
+                        });
+                        win.loadFile('main/children.html');
+                        win.on('closed', ()=>{
+                            win = null
+                        })
+                    }
+                },
+                {
+                    type: 'separator'
+                },
+                {label:'抖音热歌'},
+                {label:'车载音乐'},
+                {label:'网红歌曲'}
+            ]
+        },
+        {
+            label: '电台',
+            submenu: [
+                {label:'主题'},
+                {label:'场景'},
+                {label:'心情'}
+            ]
+        }
+    ]
+    
+    let m = Menu.buildFromTemplate(template);// 加载模板
+    
+    Menu.setApplicationMenu(m);     // 设置窗口的菜单
 
     win.on('close', (e) => {
-        //回收BrowserWindow对象
+        // 回收BrowserWindow对象
         if (win.isMinimized()) {
             win = null;
         } else {
@@ -94,10 +146,57 @@ function createWindow() {
             win.minimize();
         }
     });
-    // win.on('resize', () => {
-    //     win.reload();
-    // })
+    win.on('resize', () => {
+        win.reload();
+    });
+
+
+    // 引入BrowserView在窗口中打开外部网页，类似iframe效果
+    let BrowserView = electron.BrowserView;
+    let view = new BrowserView();
+    win.setBrowserView(view);
+    view.setBounds({
+        x: 0,
+        y: 300,
+        width: 500,
+        height: 400
+    });
+    view.webContents.loadURL('https://www.baidu.com');
+
+    // 注册全局快捷键
+    globalShortcut.register('ctrl+b', () => {
+        let win = new BrowserWindow({
+            width:500,
+            height:500,
+            webPreferences: {
+                nodeIntegration: true
+            }
+        });
+        win.loadURL('http://www.zhifou.co/');
+        win.on('closed', ()=>{
+            win = null
+        })
+    });
+
+    let isRegister= globalShortcut.isRegistered('ctrl+b') ? 'Register Success':'Register fail';
+    console.log('------->'+isRegister);
 }
+
+/**
+ * 监听最大化、最小化、关闭操作
+ */
+// ipcMain.on('min', e => {
+//     win.minimize();
+//     console.log('min');
+// });
+// ipcMain.on('max', e => {
+//     console.log('max');
+//     if (win.isMaximized() || win.isFullScreen()) {
+//         win.unmaximize();
+//     } else {
+//         win.maximize();
+//     }
+// });
 
 // 监听渲染程序发来的事件
 ipcMain.on('something', (event, data) => {
@@ -114,7 +213,13 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-    if (win == null) {
+    if (win === null) {
         createWindow();
     }
-}) 
+});
+
+app.on('will-quit', function() {
+    //注销全局快捷键的监听
+    globalShortcut.unregister('ctrl+b');
+    globalShortcut.unregisterAll();
+});
--- src/container/Home/index.tsx
+++ src/container/Home/index.tsx
@@ -1,63 +1,182 @@
-import React, { Component } from 'react';
-import { remote, ipcRenderer } from 'electron';
-import './index.scss';
-import { Button, message } from 'antd';
+import React, { Component } from "react";
+import { remote, ipcRenderer, clipboard, shell } from "electron";
+import "./index.scss";
+import { Button, message } from "antd";
+import { EventHandler } from "react";
 
-const { Notification } = remote;
+const { Notification, BrowserWindow } = remote;
 
-interface IProps {
-
-}
+interface IProps {}
 interface IState {
-  info: string;
+    info: string;
 }
 
 class Home extends Component<IProps, IState> {
-  constructor(props: IProps) {
-    super(props);
+    constructor(props: IProps) {
+        super(props);
 
-    this.state = {
-      info: ''
+        this.state = {
+            info: "",
+        };
+    }
+    componentDidMount() {
+        // 监听主进程发来的事件
+        ipcRenderer.on("fromMainProcess", (event: any, data: any) => {
+            console.log("接收到main进程发送的消息11", data); // 我是主进程返回的值
+            this.setState({
+                info: JSON.stringify(data),
+            });
+        });
+
+        // 设置应用程序右键菜单
+        let rightTemplate = [
+            {
+                label: "剪切",
+                accelerator: "ctrl+x",
+            },
+            {
+                label: "复制",
+                accelerator: "ctrl+c",
+            },
+            {
+                label: "粘贴",
+                accelerator: "ctrl+v",
+            },
+            {
+                label: "新建",
+                accelerator: "ctrl+n",
+                click: () => {
+                    let win = new BrowserWindow({
+                        width: 500,
+                        height: 500,
+                        webPreferences: {
+                            nodeIntegration: true,
+                        },
+                    });
+                    win.loadFile("main/children.html");
+                    win.on("closed", () => {
+                        win = null;
+                    });
+                },
+            },
+        ];
+
+        let mContext = remote.Menu.buildFromTemplate(rightTemplate);
+        window.addEventListener("contextmenu", function (e) {
+            // alert('码云笔记')
+            e.preventDefault();
+            mContext.popup({ window: remote.getCurrentWindow() });
+        });
+    }
+
+    componentWillUnmount() {
+        ipcRenderer.removeAllListeners("fromMainProcess");
+    }
+
+    onShowNotification = () => {
+        let myNotification = new Notification({
+            title: "渲染进程通知",
+            body: "在渲染进程中直接使用主进程的模块11",
+        });
+        myNotification.show();
     };
-  }
-  componentDidMount() {
-    // 监听主进程发来的事件
-    ipcRenderer.on('fromMainProcess', (event: any, data: any) => {
-      console.log('接收到main进程发送的消息11', data); // 我是主进程返回的值
-      this.setState({
-        info: JSON.stringify(data)
-      });
-    })
-  }
 
-  componentWillUnmount() {
-    ipcRenderer.removeAllListeners('fromMainProcess');
-  }
+    onSendMessageToMain = () => {
+        // 发送事件给主进程
+        ipcRenderer.send("something", "传输给主进程的值");
+    };
 
-  onShowNotification = () => {
-    let myNotification = new Notification({ title: '渲染进程通知', body: '在渲染进程中直接使用主进程的模块11' });
-    myNotification.show();
-  }
+    onShowInfo = () => {
+        message.success(this.state.info);
+    };
 
-  onSendMessageToMain = () => {
-    // 发送事件给主进程
-    ipcRenderer.send('something', '传输给主进程的值')
-  }
+    onCopyClick = () => {
+        const code = (document as Document).querySelector("#code");
+        clipboard.writeText(code.innerHTML);
+        message.info("复制成功");
+    };
 
-  onShowInfo = () => {
-    message.success(this.state.info);
-  }
+    onOpenNewWindow = () => {
+        let newWin = new BrowserWindow({
+            width: 500,
+            height: 500,
+        });
 
-  render() {
-    return (
-      <React.Fragment>
-        <button onClick={this.onSendMessageToMain}>与主进程通信</button>
-        <br /> <br />
-        <button onClick={this.onShowNotification}>使用 remote 直接调用主进程模块</button>
-        <Button type="primary" onClick={this.onShowInfo}>我是button</Button>
-      </React.Fragment>
-    )
-  }
+        newWin.loadFile("main/yellow.html");
+        newWin.on("closed", () => {
+            newWin = null;
+        });
+    };
+
+    // 在浏览器中打开页面
+    openInBrowser = (e: React.MouseEvent<HTMLAnchorElement>) => {
+        e.preventDefault();
+        console.log(e.target);
+        let href = (e.target as HTMLAnchorElement).href;
+        shell.openExternal(href);
+    };
+
+    onOpenSubWindow = () => {
+        window.open('https://www.baidu.com');
+    };
+
+    onSendMsg = () => {
+        let option = {
+            title: '来自海上的消息',
+            body: '欢迎来上海做客，阿拉上海欢迎你。'
+        }
+        new window.Notification(option.title, option);
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+                <div>
+                    <button onClick={this.onSendMessageToMain}>
+                        与主进程通信
+                    </button>
+                </div>
+                <div>
+                    <button onClick={this.onShowNotification}>
+                        使用 remote 直接调用主进程模块
+                    </button>
+                </div>
+                <div>
+                    <Button type="primary" onClick={this.onShowInfo}>
+                        我是button
+                    </Button>
+                </div>
+                <div>
+                    激活码: <span id="code">mybj123com1234567</span>
+                    <Button type="primary" onClick={this.onCopyClick}>
+                        复制激活码
+                    </Button>
+                </div>
+                <Button type="primary" onClick={this.onOpenNewWindow}>
+                    打开一个新窗口
+                </Button>
+                {/** 如何在浏览器中打开链接 */}
+                <div>
+                    <a
+                        onClick={this.openInBrowser}
+                        href="https://www.baidu.com"
+                    >
+                        在浏览器中打开百度
+                    </a>
+                </div>
+                <div>
+                    <Button type="primary" onClick={this.onOpenSubWindow}>
+                        打开一个子窗口
+                    </Button>
+                </div>
+                <div>
+                    <Button type="primary" onClick={this.onSendMsg}>
+                        发送一个消息
+                    </Button>
+                </div>
+            </React.Fragment>
+        );
+    }
 }
 
 export default Home;
